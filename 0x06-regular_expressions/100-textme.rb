66d3e2d7befefea8cbaf312782154bc0e3df0741
walebign3 <walebign3@gmail.com>
2022-08-24 21:11:23 +0300

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:)(\+?[a-zA-Z0-9]+)|(?<=to:)(\+?[0-9a-zA-Z]+)|(?<=flags:)(-?[0-1]:-?[0-1]:-?[0-1]:-?[0-1]:-?[0-1])/).join
+puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')

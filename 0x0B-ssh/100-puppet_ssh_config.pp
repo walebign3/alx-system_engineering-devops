81dbbadcaf4abdd561668fbf04ae199b93f8d22f
walebign3 <walebign3@gmail.com>
2022-10-19 21:02:00 +0300

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # Update SSH configuration file
-file_line { 'Update private key':
+file_line { 'Change the main private key':
   path => '/etc/ssh/ssh_config',
   line => '    IdentityFile ~/.ssh/school',
 }
 
-file_line { 'Update authentication method':
+file_line { 'No Authenticate with passowrd':
   path => '/etc/ssh/ssh_config',
   line => '    PasswordAuthentication no',
 }

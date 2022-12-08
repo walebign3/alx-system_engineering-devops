4e823d711bc30e9a10bbd1488fe7471717e09108
walebign3 <walebign3@gmail.com>
2022-12-08 09:53:52 +0300

    1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,11 @@
 # Change the open file limit for the current user
-exec { 'increase performance':
-  command => 'ulimit -n 5000'
-  path    => '/bin:/usr/bin:/usr/sbin'
+exec { 'increase hard file limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for current user.
+exec { 'increase soft file limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
